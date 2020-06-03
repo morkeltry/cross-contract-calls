b499a598f70f1aa2b831d5642c264ee1884940b4
Mork <github@morkeltry.33mail.com>
2020-06-03 12:41:26 +0100

    changed accidental bytes casting to int to casting to uint

--- contracts/stringCasting.sol
+++ contracts/stringCasting.sol
@@ -6,8 +6,8 @@ library stringcast{
 		bytes memory bStr = bytes(str);
 		bytes memory bLower = new bytes(bStr.length);
 		for (uint i = 0; i < bStr.length; i++) {
-			if ((int8(bStr[i]) >= 65) && (int8(bStr[i]) <= 90)) {
-				bLower[i] = byte(int8(bStr[i]) + 32);
+			if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
+				bLower[i] = byte(uint8(bStr[i]) + 32);
 			} else {
 				bLower[i] = bStr[i];
 			}
@@ -19,8 +19,8 @@ library stringcast{
 		bytes memory bStr = bytes(str);
 		bytes memory bUpper = new bytes(bStr.length);
 		for (uint i = 0; i < bStr.length; i++) {
-			if ((int8(bStr[i]) >= 97) && (int8(bStr[i]) <= 122)) {
-				bUpper[i] = byte(int8(bStr[i]) - 32);
+			if ((uint8(bStr[i]) >= 97) && (uint8(bStr[i]) <= 122)) {
+				bUpper[i] = byte(uint8(bStr[i]) - 32);
 			} else {
 				bUpper[i] = bStr[i];
 			}
